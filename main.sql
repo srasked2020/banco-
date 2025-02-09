8b561e39bd3d29c4e915806c15839f6d51e107bf
srasked2020 <asked8104@gmail.com>
2025-02-09 10:10:37 -0300

    Update main.sql

--- main.sql
+++ main.sql
@@ -5,11 +5,24 @@ CREATE DATABASE farmacia;
 
 CREATE TABLE clientes(
 
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     Nome VARCHAR(100) NOT NULL,
-    Email VARCHAR(45) UNIQUE NOT NULL,
-    senha VARCHAR(16)NOT NULL,
-    DATA_DE_NACIMENTO DATE NOT NULL
+    CPF_CNPJ VARCHAR(25),
+    TIPO_DE_CLIENTE ENUM('PessoaFisica', 'PessoaJuridica'),
+    UNIQUE (CPF_CNPJ, TIPO_DE_CLIENTE )
+    
 );
 
+/*INSERT INTO clientes (Nome, CPF_CNPJ, TIPO_DE_CLIENTE) VALUES ('thomasturbando','567.202.150-60','PessoaFisica')*/
+
+CREATE TABLE produtos(
+
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    Nome_produto VARCHAR(255),
+    preço VARCHAR(25),
+    Tipo_de_produto('cosmetico', 'higiene', 'remedio'),
+    UNIQUE(Tipo_de_produto)
+);
+
+INSERT INTO produtos(Nome_produto, Tipo_de_produto,)
 
